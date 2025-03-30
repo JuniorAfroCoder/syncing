07216e7b5cf08a79862e30814a7556f31747e54e
JuniorAfroCoder <murengukojesusd@gmail.com>
2025-03-30 15:31:24 +0200

    updated credentials handling for security purposes

--- server.js
+++ server.js
@@ -1,17 +1,38 @@
 require('dotenv').config();
 const { google } = require('googleapis');
+const { GoogleAuth } = require('google-auth-library');
 const { MongoClient } = require('mongodb');
 
 // Load Google Sheets credentials
-const auth = new google.auth.GoogleAuth({
-  keyFile: 'credentials.json',
-  scopes: ['https://www.googleapis.com/auth/spreadsheets']
+const auth = new GoogleAuth({
+  credentials: {
+    type: process.env.GOOGLE_TYPE,
+    project_id: process.env.GOOGLE_PROJECT_ID,
+    private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    client_id: process.env.GOOGLE_CLIENT_ID,
+  },
+  scopes: ['https://www.googleapis.com/auth/spreadsheets'],
 });
+
 const sheets = google.sheets({ version: 'v4', auth });
 
 const MONGO_URI = process.env.MONGO_URI;
 const SHEET_ID = process.env.SHEET_ID;
 const DATABASE_NAME = process.env.DATABASE_NAME;
+const credentials = {
+  type: process.env.GOOGLE_TYPE,
+  project_id: process.env.GOOGLE_PROJECT_ID,
+  private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+  private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.GOOGLE_CLIENT_EMAIL,
+  client_id: process.env.GOOGLE_CLIENT_ID,
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url: `https://www.googleapis.com/robot/v1/metadata/x509/${process.env.GOOGLE_CLIENT_EMAIL}`
+};
 
 async function getCollectionNames(client) {
   const db = client.db(DATABASE_NAME);
